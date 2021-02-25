ee48f2aa63d2e8cc370e236eeb43b4617c99eac9
riulsudario <riulsudario@jera.com.br>
2021-02-25 15:47:15 -0400

    [FIX] fixing SEO

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -25,11 +25,18 @@ export function Overview(props: pokemonDetails){
   return(
     <div className={styles.overviewContainer}>
       <Head>
-        <meta property="og:image" content="http://pokedollar.herokuapp.com/pokedollar.png" />
+        <meta property="og:image"         content="http://pokedollar.herokuapp.com/pokedollar.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1920" />
+        <meta property="og:image:height" content="1080" />
+        <meta property="og:image:alt" content="Pokedollar" />
+
         <meta property="og:url"           content={url} />
         <meta property="og:type"          content="website" />
         <meta property="og:title"         content={title} />
         <meta property="og:description"   content={description} />
+        <meta property="og:locale"        content="pt_BR" />
+        <meta property="og:site_name"     content="Pokédollar"></meta>
 
         <meta name="twitter:title"        content={title} />
         <meta name="twitter:description"  content={description} />
