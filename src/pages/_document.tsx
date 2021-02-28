27d52583361dde7369568dae6db76228fe6c48de
riulsudario <riulsudario@jera.com.br>
2021-02-28 00:03:17 -0400

    [NEW] adding schema.org

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,30 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render () {
+    const schema = {
+      "@context": "http://schema.org",
+      "@type": "WebSite",
+      "url": "https://pokedollar.herokuapp.com/",
+      "potentialAction": {
+        "@type": "ViewAction",
+          "target": [
+            "https://pokedollar.herokuapp.com/dollar",
+            {
+              "@type": "EntryPoint",
+              "urlTemplate": "https://pokedollar.herokuapp.com/dollar",
+              "contentType": "application/json+ld"
+            },
+          ]
+        }
+      }
+
     return (
       <Html lang="pt-BR">
-        <Head />
+        <Head>
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{__html: JSON.stringify(schema)}} />
+        </Head>
 
         <body>
           <Main />
